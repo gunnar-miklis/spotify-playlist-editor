0ff83f3e805cda55b7f8d7950abe216f21483da2
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-05 23:53:43 +0200

    adjust initial volume

--- src/components/playlist/AudioPlayback.tsx
+++ src/components/playlist/AudioPlayback.tsx
@@ -1,25 +1,29 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 'use client';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MdOutlinePlayCircle, MdOutlineMotionPhotosPaused } from 'react-icons/md';
 import styles from '@/src/styles/app.module.css';
 
-type Props = {
-  source: string;
-};
-
+type Props = { source: string };
 export default function AudioPlayback({ source }: Props) {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null!);
+
+  useEffect(() => {
+    if (isPlaying) audioRef.current.volume = 0.5;
+  }, [isPlaying]);
+
   const handlePlayPause = () => {
     if (!isPlaying) {
-      // stop all other audio
       const allAudios = document.querySelectorAll('audio');
+
+      // stop all other audio
       allAudios.forEach((audio) => {
         audio.pause();
         audio.currentTime = 0;
       });
+
       // play the clicked audio only
       audioRef.current.play();
     } else {
