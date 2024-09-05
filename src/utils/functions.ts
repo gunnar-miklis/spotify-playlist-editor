38eaf86c3bc86f786ada138aea0577d86ec35d6e
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-06 00:01:19 +0200

    describe functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,13 @@
 import type { Dispatch, SetStateAction } from 'react';
 
+/**
+ * Sorts an array of objects based on a field and toggles the sort order between ascending and descending.
+ * @param field the field to sort by.
+ * @param initalArray the array to sort.
+ * @param setSortedArray the setter for the sorted array.
+ * @param isAsc whether to sort in ascending or descending order.
+ * @param setIsAsc the setter for isAsc.
+ */
 export function sortObjectsByField<ObjectType>(
   field: keyof ObjectType,
   initalArray: ObjectType[],
@@ -23,8 +31,13 @@ export function sortObjectsByField<ObjectType>(
   setIsAsc(isAsc ? false : true);
 }
 
+/**
+ * Converts milliseconds to minutes and seconds.
+ * @param ms the number of milliseconds.
+ * @returns a string in the format of '02:05'.
+ */
 export function msToMin(ms: number): string {
   const minutes: number = Math.floor(ms / 60 / 1000);
   const seconds: string = ((ms % 60000) / 1000).toFixed(0);
-  return `${minutes}:${seconds.padStart(2, '0')}`;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.padStart(2, '0')}`;
 }
